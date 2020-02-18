29a8a3769c54fc7c44dea91ebc7f7c4b84b87171
Yuki (aka Ruben Gomez) <rugoli@gmail.com>
2020-02-19 00:03:10 +0100

    Initial Commit

---
+++ Appnea WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Appnea WatchKit Extension
+//
+//  Created by Rubén Gómez Olivencia on 19/02/2020.
+//  Copyright © 2020 Rubén Gómez Olivencia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Appnea WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Appnea WatchKit Extension
+//
+//  Created by Rubén Gómez Olivencia on 19/02/2020.
+//  Copyright © 2020 Rubén Gómez Olivencia. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
